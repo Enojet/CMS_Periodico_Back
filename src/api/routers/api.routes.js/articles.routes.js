e2cabd7e0e2e4a2b5d7da42c3997455809a0a706
Jone Etxaburu <etxaburu.garai@gmil.com>
2025-01-21 20:01:29 +0100

    estructura proyecto

--- src/api/routers/api.routes.js/articles.routes.js
+++ src/api/routers/api.routes.js/articles.routes.js
@@ -1,8 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const { addUser, getUsers } = require('../controllers/user.controller');
-
-router.post('/add', addUser);
-router.get('/listusers', getUsers);
-
-module.exports = router;
+//articulo
