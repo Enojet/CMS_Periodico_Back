12756df4ec130091cc4e2223129f147669c5fb0a
Jone Etxaburu <etxaburu.garai@gmil.com>
2025-01-31 17:15:26 +0100

    Cloudinary para funcion de create y updateContent

--- src/api/routers/api/articles.routes.js
+++ src/api/routers/api/articles.routes.js
@@ -17,18 +17,18 @@ const upload=require("../../../utils/upload.file");
 //Ruta para obetener todos los articulos y un articulo concreto por su id 
 router.get('/publishArticles', allPublishArticles);//todos los articulos publicados ordenados
 //WRITER
-router.post('/create', authMiddleware, createArticle);
+router.post('/create', authMiddleware,upload.single("image"), createArticle);
 router.get('/getArticlesByAuthor/:idAutor', authMiddleware, articlesByAuthor);//muestra todos los artículos del id de ese autor
 
 router.get('/getArticlesByEditor/:idEditor', authMiddleware, articlesByEditor);//-todos los articulos que tengan el id del editor
 router.get('/articleById/:IdA', authMiddleware, detailArticleById);// se muestra el articulo seleccionado (cambiar /:id)
 //Ruta para modificación del articulo:contenido,status, asignar editor
-router.put('/content/:id', authMiddleware, updateArticleById);//Editar contenido según  el Id
+router.put('/content/:id', authMiddleware, upload.single("image"), updateArticleById);//Editar contenido según  el Id
 router.put('/status/:id/:status', authMiddleware, updateStatus);//Editar el status revisable, draft, publicar
 //COMPROBAR RUTA
 router.put('/asignEditor/:idA/:idE', authMiddleware, asignEditor);//Asignar un editor
 
-router.post('/uploadFile',upload.single("image"), imageUpload);
+//router.post('/uploadFile',upload.single("image"), imageUpload);
 
 
 
